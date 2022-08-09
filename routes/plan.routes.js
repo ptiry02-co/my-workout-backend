efc9c71803c22d1a03f5ccfef33c70a148d7bec7
Paul Tiry <tirypaul@gmail.com>
2022-08-09 20:29:31 +0200

    finished CRUD paths for plans

--- routes/plan.routes.js
+++ routes/plan.routes.js
@@ -29,4 +29,41 @@ router.post('/', isAuthenticated, async (req, res, next) => {
     console.log('Error creating new plan: ', error)
   }
 })
+
+router.get('/:planId', isAuthenticated, async (req, res, next) => {
+  const { planId } = req.params
+
+  try {
+    const plan = await Plan.findById(planId).populate('exercises')
+
+    res.json(plan)
+  } catch (error) {
+    console.log('Error fetching plan: ', error)
+  }
+})
+
+router.put('/:planId', isAuthenticated, async (req, res, next) => {
+  const newInfo = req.body
+  const { planId } = req.params
+
+  try {
+    const editedPlan = await Plan.findByIdAndUpdate(planId, newInfo, { new: true })
+
+    res.json(editedPlan)
+  } catch (error) {
+    console.log('Error creating new plan: ', error)
+  }
+})
+
+router.delete('/:planId', isAuthenticated, async (req, res, next) => {
+  const { planId } = req.params
+
+  try {
+    await Plan.findByIdAndDelete(planId)
+
+    res.json({ message: 'Delete Successfull' })
+  } catch (error) {
+    console.log('Error deleting plan: ', error)
+  }
+})
 module.exports = router
