ed6181469c9d2a0f87c9645c0dbf4e79589f33c1
Paul Tiry <tirypaul@gmail.com>
2022-12-01 11:30:32 +0000

    reset routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -8,12 +8,8 @@ router.get('/', async (req, res, next) => {
   res.json('All good in here')
 })
 
-router.get('http://13.39.13.247/.well-known/pki-validation/9F94B70D0B9965576B49A310172D996D.txt', (req, res) => {
-  res.sendFile('/home/ubuntu/my-workout-backend/config/9F94B70D0B9965576B49A310172D996D.txt')
-})
-
-router.use('/api/auth', authRoutes)
-router.use('/api/plans', planRoutes)
-router.use('/api/exercises', exerciseRoutes)
+router.use('/auth', authRoutes)
+router.use('/plans', planRoutes)
+router.use('/exercises', exerciseRoutes)
 
 module.exports = router
