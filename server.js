601110444c2c8817ec8d1ffcfc66e0c5e7cb8cb2
Paul Tiry <tirypaul@gmail.com>
2022-12-01 12:59:42 +0000

    made https connection

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const app = require('./app')
 
+const fs = require('fs')
+const https = require('https')
+
+const key = fs.readFileSync('/home/ubuntu/my-workout-backend/config/private.key')
+const cert = fs.readFileSync('/home/ubuntu/my-workout-backend/config/certificate.crt')
+
+const cred = {
+  key,
+  cert
+}
+
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5006
-const PORT = process.env.PORT || 5006
+/* const PORT = process.env.PORT || 80
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`)
+  console.log(`Server listening on port: ${PORT}`)
+}) */
+
+const httpsServer = https.createServer(cred, app)
+httpsServer.listen(443, () => {
+  console.log(`Secure server listening on port: 443`)
 })
