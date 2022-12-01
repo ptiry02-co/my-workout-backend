ac78e1349a54e76ff34a4b2bc4b44e692d04eeec
Paul Tiry <tirypaul@gmail.com>
2022-12-01 11:50:12 +0100

    setup for deploy to aws server

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -8,8 +8,12 @@ router.get('/', async (req, res, next) => {
   res.json('All good in here')
 })
 
-router.use('/auth', authRoutes)
-router.use('/plans', planRoutes)
-router.use('/exercises', exerciseRoutes)
+router.get('http://13.39.13.247/.well-known/pki-validation/9F94B70D0B9965576B49A310172D996D.txt', (req, res) => {
+  res.sendFile('/Users/paultiry/Developer/aws/9F94B70D0B9965576B49A310172D996D.txt')
+})
+
+router.use('/api/auth', authRoutes)
+router.use('/api/plans', planRoutes)
+router.use('/api/exercises', exerciseRoutes)
 
 module.exports = router
