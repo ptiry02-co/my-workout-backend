fb41fb9a5624268f1985ddd144e78787ace1c928
Paul Tiry <tirypaul@gmail.com>
2022-12-01 16:11:15 +0100

    removed https server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
-const fs = require('fs')
+/* const fs = require('fs')
 const https = require('https')
 
 const key = fs.readFileSync('/home/ubuntu/my-workout-backend/config/private.key')
@@ -9,18 +9,18 @@ const cert = fs.readFileSync('/home/ubuntu/my-workout-backend/config/certificate
 const cred = {
   key,
   cert
-}
+} */
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5006
-/* const HTTP_PORT = process.env.PORT || 8080
+const HTTP_PORT = process.env.HTTP_PORT || 8080
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`)
-}) */
+  console.log(`Server listening on port: ${HTTP_PORT}`)
+})
 
-const HTTPS_PORT = process.env.HTTPS_PORT
+/* const HTTPS_PORT = process.env.HTTPS_PORT
 
 const httpsServer = https.createServer(cred, app)
 httpsServer.listen(HTTPS_PORT, () => {
   console.log(`Secure server listening on port: ${HTTPS_PORT}`)
-})
+}) */
