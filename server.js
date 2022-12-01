cb900ccd26ebbb34519780ce09ff56d51477906b
Paul Tiry <tirypaul@gmail.com>
2022-12-01 16:18:39 +0100

    fixed port variable name

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const cred = {
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5006
 const HTTP_PORT = process.env.HTTP_PORT || 8080
 
-app.listen(PORT, () => {
+app.listen(HTTP_PORT, () => {
   console.log(`Server listening on port: ${HTTP_PORT}`)
 })
 
